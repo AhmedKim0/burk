313ded643acd723518b3096e1b1701113dabcbd0
Mostafa Mohsen <mmohsen@getgroup.com>
2024-11-21 20:56:15 +0200

    entitys

---
+++ Burk.DAL/Entity/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Burk.DAL.Interfaces;
+
+namespace Burk.DAL.Entity
+{
+       public class Client:BaseAuditableEntity,IAuditable
+    {
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string? Email { get; set; }
+      
+        public List<Review> Reviews { get; set; } //Nav property one to many
+        public List<AvaliableTabel> AvaliableTabel { get; set; }//Nav property one to many
+
+
+    }
+}
